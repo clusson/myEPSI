87aa443c5ea2467418aba47c829fc0fe049ecbdf
Adrien LECELLIER <lecellier.adrien@gmail.com>
2017-05-22 17:51:43 +0200

    Modification du Beans status
    Modification du Beans status

--- src/fr/epsi/myEpsi/beans/Status.java
+++ src/fr/epsi/myEpsi/beans/Status.java
@@ -1,6 +1,18 @@
 package fr.epsi.myEpsi.beans;
 
+import java.util.ArrayList;
+
 public enum Status {
 
 	PUBLIC, PRIVATE, ARCHIVED;
+	
+	private static Status[] status = values();
+	public static Status fromOrdinal(int n) {return status[n];}
+	public static ArrayList<Status> getList(){
+		ArrayList AL = new ArrayList();	
+		AL.add(PUBLIC);
+		AL.add(PRIVATE);
+		AL.add(ARCHIVED);
+		return AL;
+	}
 }
