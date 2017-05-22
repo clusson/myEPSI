7e69c2042653b010b86675bdd936c9386b38981a
Clément Lusson <clementlusson@gmail.com>
2017-05-22 16:40:55 +0200

    update sign servlet

--- src/fr/epsi/myEpsi/servlet/SignServlet.java
+++ src/fr/epsi/myEpsi/servlet/SignServlet.java
@@ -19,7 +19,7 @@ import fr.epsi.myEpsi.service.UserService;
 /**
  * Servlet implementation class SignServlet
  */
-@WebServlet("/sign")
+@WebServlet("sign")
 public class SignServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String LIST_USER = "/listUser.jsp";
